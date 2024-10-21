886206fb3928c7307b1204c66e86c62213fb9a41
Navin Karthik <navin.s.karthik@gmail.com>
2024-10-20 20:38:37 -0600

    Refactored code to use private functions for DCMotor and Servo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -1,12 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 // teleop start
     @TeleOp
     public class MecanumTeleOp extends LinearOpMode {
@@ -22,15 +20,18 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
         @Override
         public void runOpMode() {
 
-            frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-            frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-            backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
-            backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+//            frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+//            frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+//            backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+//            backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
 
-            frontLeftServo = hardwareMap.get(Servo.class, "frontLeftServo");
-            frontRightServo = hardwareMap.get(Servo.class, "frontRightServo");
-            backLeftServo = hardwareMap.get(Servo.class, "backLeftServo");
-            backRightServo = hardwareMap.get(Servo.class, "backRightServo");
+//            frontLeftServo = hardwareMap.get(Servo.class, "frontLeftServo");
+//            frontRightServo = hardwareMap.get(Servo.class, "frontRightServo");
+//            backLeftServo = hardwareMap.get(Servo.class, "backLeftServo");
+//            backRightServo = hardwareMap.get(Servo.class, "backRightServo");
+
+            this.initializeDcMotors();
+            this.initializeServos();
 
             telemetry.addData("Status", "Initialized");
             telemetry.update();
@@ -45,16 +46,18 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
                 double tgtPower = 0;
                 while (opModeIsActive()) {
                     tgtPower = -this.gamepad1.left_stick_y;
-                    frontLeftMotor.setPower(tgtPower);
-                    frontRightMotor.setPower(tgtPower);
-                    backLeftMotor.setPower(tgtPower);
-                    backRightMotor.setPower(tgtPower);
+//                    frontLeftMotor.setPower(tgtPower);
+//                    frontRightMotor.setPower(tgtPower);
+//                    backLeftMotor.setPower(tgtPower);
+//                    backRightMotor.setPower(tgtPower);
+                    this.setDcMotorTargetPower(tgtPower);
 
                     telemetry.addData("Target Power", tgtPower);
-                    telemetry.addData("Motor Power", frontLeftMotor.getPower());
-                    telemetry.addData("Motor Power", frontRightMotor.getPower());
-                    telemetry.addData("Motor Power", backLeftMotor.getPower());
-                    telemetry.addData("Motor Power", backRightMotor.getPower());
+//                    telemetry.addData("Motor Power", frontLeftMotor.getPower());
+//                    telemetry.addData("Motor Power", frontRightMotor.getPower());
+//                    telemetry.addData("Motor Power", backLeftMotor.getPower());
+//                    telemetry.addData("Motor Power", backRightMotor.getPower());
+                    this.telemetryDcMotorPower();
 
                     telemetry.addData("Status", "Running");
                     telemetry.update();
@@ -62,41 +65,47 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
                     tgtPower = 0;
                     while (opModeIsActive()) {
                         tgtPower = -this.gamepad1.left_stick_y;
-                        frontLeftMotor.setPower(tgtPower);
-                        frontRightMotor.setPower(tgtPower);
-                        backLeftMotor.setPower(tgtPower);
-                        backRightMotor.setPower(tgtPower);
+//                        frontLeftMotor.setPower(tgtPower);
+//                        frontRightMotor.setPower(tgtPower);
+//                        backLeftMotor.setPower(tgtPower);
+//                        backRightMotor.setPower(tgtPower);
+                        this.setDcMotorTargetPower(tgtPower);
 
                         // check to see if we need to move the servo.
                         if(gamepad1.y) {
                             // move to 0 degrees.
-                            frontRightServo.setPosition(0);
-                            frontLeftServo.setPosition(0);
-                            backLeftServo.setPosition(0);
-                            backRightServo.setPosition(0);
+//                            frontRightServo.setPosition(0);
+//                            frontLeftServo.setPosition(0);
+//                            backLeftServo.setPosition(0);
+//                            backRightServo.setPosition(0);
+                            this.setServoPosition(0);
                         } else if (gamepad1.b|| gamepad1.x) {
                             // move to 90 degrees.
-                            frontRightServo.setPosition(0.5);
-                            frontLeftServo.setPosition(0.5);
-                            backLeftServo.setPosition(0.5);
-                            backRightServo.setPosition(0.5);
+//                            frontRightServo.setPosition(0.5);
+//                            frontLeftServo.setPosition(0.5);
+//                            backLeftServo.setPosition(0.5);
+//                            backRightServo.setPosition(0.5);
+                            this.setServoPosition(0.5);
                         } else if (gamepad1.a) {
                             // move to 180 degrees.
-                            frontRightServo.setPosition(1);
-                            frontLeftServo.setPosition(1);
-                            backLeftServo.setPosition(1);
-                            backRightServo.setPosition(1);
+//                            frontRightServo.setPosition(1);
+//                            frontLeftServo.setPosition(1);
+//                            backLeftServo.setPosition(1);
+//                            backRightServo.setPosition(1);
+                            this.setServoPosition(1);
                         }
 
-                        telemetry.addData("Servo Position", frontLeftServo.getPosition());
-                        telemetry.addData("Servo Position", frontRightServo.getPosition());
-                        telemetry.addData("Servo Position", backLeftServo.getPosition());
-                        telemetry.addData("Servo Position", backRightServo.getPosition());
+//                        telemetry.addData("Servo Position", frontLeftServo.getPosition());
+//                        telemetry.addData("Servo Position", frontRightServo.getPosition());
+//                        telemetry.addData("Servo Position", backLeftServo.getPosition());
+//                        telemetry.addData("Servo Position", backRightServo.getPosition());
+                        this.telemetryServoPosition();
                         telemetry.addData("Target Power", tgtPower);
-                        telemetry.addData("Motor Power", frontLeftMotor.getPower());
-                        telemetry.addData("Motor Power", frontRightMotor.getPower());
-                        telemetry.addData("Motor Power", backLeftMotor.getPower());
-                        telemetry.addData("Motor Power", backRightMotor.getPower());
+//                        telemetry.addData("Motor Power", frontLeftMotor.getPower());
+//                        telemetry.addData("Motor Power", frontRightMotor.getPower());
+//                        telemetry.addData("Motor Power", backLeftMotor.getPower());
+//                        telemetry.addData("Motor Power", backRightMotor.getPower());
+                        this.telemetryDcMotorPower();
 
                         telemetry.addData("Status", "Running");
                         telemetry.update();
@@ -105,4 +114,51 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
             }
         }
 
+        private void initializeDcMotors()
+        {
+            frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+            frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+            backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+            backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        }
+
+        private void initializeServos()
+        {
+            frontLeftServo = hardwareMap.get(Servo.class, "frontLeftServo");
+            frontRightServo = hardwareMap.get(Servo.class, "frontRightServo");
+            backLeftServo = hardwareMap.get(Servo.class, "backLeftServo");
+            backRightServo = hardwareMap.get(Servo.class, "backRightServo");
+        }
+
+        private void setDcMotorTargetPower(double tgtPower)
+        {
+            frontLeftMotor.setPower(tgtPower);
+            frontRightMotor.setPower(tgtPower);
+            backLeftMotor.setPower(tgtPower);
+            backRightMotor.setPower(tgtPower);
+        }
+
+        private void setServoPosition(double degrees)
+        {
+            frontRightServo.setPosition(degrees);
+            frontLeftServo.setPosition(degrees);
+            backLeftServo.setPosition(degrees);
+            backRightServo.setPosition(degrees);
+        }
+
+        private void telemetryServoPosition()
+        {
+            telemetry.addData("Servo Position", frontLeftServo.getPosition());
+            telemetry.addData("Servo Position", frontRightServo.getPosition());
+            telemetry.addData("Servo Position", backLeftServo.getPosition());
+            telemetry.addData("Servo Position", backRightServo.getPosition());
+        }
+
+        private void telemetryDcMotorPower()
+        {
+            telemetry.addData("Motor Power", frontLeftMotor.getPower());
+            telemetry.addData("Motor Power", frontRightMotor.getPower());
+            telemetry.addData("Motor Power", backLeftMotor.getPower());
+            telemetry.addData("Motor Power", backRightMotor.getPower());
+        }
     }
