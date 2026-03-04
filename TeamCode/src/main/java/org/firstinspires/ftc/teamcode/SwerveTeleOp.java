61b6d3b05303415b287c7274596c011963e21752
jfk12345678 <143293092+jfk12345678@users.noreply.github.com>
2026-03-03 19:24:17 -0700

    g

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveTeleOp.java
@@ -220,10 +220,12 @@ public class SwerveTeleOp extends LinearOpMode {
                 if (timer.milliseconds() > 0 && timer.milliseconds() <= 1000 && gamepad1.x) {
                     topFlap.setPosition(topFlapKick);
                     bottomFlap.setPosition(bottomFlapStow);
-                    intake.setPower(0);
-                    intakeMotor.setPower(0);
+                    intake.setPower(-1);
+                    intakeMotor.setPower(-1);
                 } else if (timer.milliseconds() > 1000 && timer.milliseconds() <= 1500 && gamepad1.x) {
                     topFlap.setPosition(topFlapStow);
+                    intake.setPower(0);
+                    intakeMotor.setPower(0);
                 } else if (timer.milliseconds() > 1500 && timer.milliseconds() <= 2000 && gamepad1.x) {
                     topFlap.setPosition(topFlapStow);
                     bottomFlap.setPosition(bottomFlapAgitate);
